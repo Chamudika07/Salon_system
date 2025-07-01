f67f04a1fa0cf7df5e3236ea3c0cfe3d0ed3b2e4
Chamudika07 <214651110+Chamudika07@users.noreply.github.com>
2025-07-01 23:02:46 +0530

    give employ to permission

--- app/api/booking.py
+++ app/api/booking.py
@@ -5,17 +5,17 @@ from app.crud.booking import (
     create_booking, get_booking, get_bookings, update_booking, delete_booking
 )
 from app.db.dependency import get_db
-
+from app.core.deps import get_current_user , get_current_active_user 
 router = APIRouter()
 
 #create booking API
 @router.post("/", response_model=BookingOut)
-def create(booking: BookingCreate, db: Session = Depends(get_db)):
+def create(booking: BookingCreate, db: Session = Depends(get_db) , current_user = Depends(get_current_active_user)):
     return create_booking(db, booking)
 
 #get bookings API
 @router.get("/", response_model=list[BookingOut])
-def list_bookings(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def list_bookings(skip: int = 0, limit: int = 10, db: Session = Depends(get_db) , current_user = Depends(get_current_active_user)):
     return get_bookings(db, skip=skip, limit=limit)
 
 #get with id booking API
--- app/api/employee.py
+++ app/api/employee.py
@@ -10,17 +10,17 @@ router = APIRouter()
 
 #employee create api
 @router.post("/", response_model=EmployeeOut)
-def create(employee: EmployeeCreate, db: Session = Depends(get_db)):
+def create(employee: EmployeeCreate, db: Session = Depends(get_db) , current_user = Depends(get_current_admin_user)):
     return create_employee(db, employee)
 
 #employees get api
 @router.get("/", response_model=list[EmployeeOut])
-def list_employees(skip: int = 0, limit: int = 10, db: Session = Depends(get_db) , current_user = Depends(get_current_active_user)):
+def list_employees(skip: int = 0, limit: int = 10, db: Session = Depends(get_db) , current_user = Depends(get_current_admin_user)):
     return get_employees(db, skip=skip, limit=limit)
 
 #employee get using id api
 @router.get("/{employee_id}", response_model=EmployeeOut)
-def read_employee(employee_id: int, db: Session = Depends(get_db)):
+def read_employee(employee_id: int, db: Session = Depends(get_db) , current_user = Depends(get_current_admin_user)):
     employee = get_employee(db, employee_id)
     if not employee:
         raise HTTPException(status_code=404, detail="Employee not found")
@@ -28,7 +28,7 @@ def read_employee(employee_id: int, db: Session = Depends(get_db)):
 
 #employee update with id api
 @router.put("/{employee_id}", response_model=EmployeeOut)
-def update(employee_id: int, employee: EmployeeCreate, db: Session = Depends(get_db)):
+def update(employee_id: int, employee: EmployeeCreate, db: Session = Depends(get_db) , current_user = Depends(get_current_admin_user)):
     updated = update_employee(db, employee_id, employee)
     if not updated:
         raise HTTPException(status_code=404, detail="Employee not found")
@@ -36,7 +36,7 @@ def update(employee_id: int, employee: EmployeeCreate, db: Session = Depends(get
 
 #employee delet with id api
 @router.delete("/{employee_id}")
-def delete(employee_id: int, db: Session = Depends(get_db) , current_admin = Depends(get_current_admin_user)):
+def delete(employee_id: int, db: Session = Depends(get_db) , current_user = Depends(get_current_admin_user)):
     deleted = delete_employee(db, employee_id)
     if not deleted:
         raise HTTPException(status_code=404, detail="Employee not found")
